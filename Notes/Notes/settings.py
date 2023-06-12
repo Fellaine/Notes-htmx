fa2679bb2b9460207dfb5b78de166a6a86681cb8
Fellaine <max.goncharevych@gmail.com>
2023-06-12 10:55:29 +0300

    Started getting ALLOWED_HOSTS and DEBUG from env

--- Notes/Notes/settings.py
+++ Notes/Notes/settings.py
@@ -29,9 +29,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = str(os.environ.get('DEBUG')) == '1'
-DEBUG = True
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS")]
 
 
 # Application definition
